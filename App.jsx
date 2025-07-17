fcfdd73a48cf57430081de1a173ba29218ba32f0
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-17 21:50:37 +0530

    roll dice button

--- App.jsx
+++ App.jsx
@@ -6,12 +6,16 @@ export default function App() {
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
   }
+  function rollDice() {
+    setDice(generateAllNewDice());
+  }
   const diceElements = dice.map((num) => <Die value={num} />);
   return (
     <main>
-      <div className="dice-container">
-        {diceElements}
-      </div>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll Dice{" "}
+      </button>
     </main>
   );
 }
