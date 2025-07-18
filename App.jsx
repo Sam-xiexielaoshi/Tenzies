5490f58a93994f679ce388a621517ba8e2977674
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-18 21:27:38 +0530

    added new game and end game state

--- App.jsx
+++ App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
+
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
@@ -35,6 +40,7 @@ export default function App() {
   ));
   return (
     <main>
+      {gameWon && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Clck each die to freeze it at its
@@ -42,7 +48,7 @@ export default function App() {
       </p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
-        Roll Dice{" "}
+        {gameWon ? "New Game" : "Roll Dice"}
       </button>
     </main>
   );
