8947f8a8b0cf71ffc05a323d737dc2f0f8985ebf
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-18 20:32:42 +0530

    styling the held dice

--- App.jsx
+++ App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
-      isHeld: false,
+      isHeld: true,
       id: nanoid(),
     }));
   }
@@ -15,7 +15,7 @@ export default function App() {
     setDice(generateAllNewDice());
   }
   const diceElements = dice.map((dieObj) => (
-    <Die key={dieObj.id} value={dieObj.value} />
+    <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld}/>
   ));
   return (
     <main>
